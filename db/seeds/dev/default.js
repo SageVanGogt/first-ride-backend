0e74cb77c0164d51d744c443ccaef9c164a10a86
Sage Vogt <thurmanvogt@gmail.com>
2018-07-14 15:40:46 -0600

    Add seeding config to knexfix for production

--- db/seeds/dev/default.js
+++ db/seeds/dev/default.js
@@ -8,6 +8,12 @@ exports.seed = function(knex, Promise) {
           location_name: 'Red Rocks', 
           location_lat_lng: '39.6654, 105.2057'
         }),
+        knex('users').insert({
+          id: 1, 
+          user_name: 'thomas',
+          email: 'thurmanvogt@gmail.com',
+          password: 'sage'
+        }),
         knex('rides').insert({
           id: 1, 
           location_id: 1,
@@ -18,20 +24,19 @@ exports.seed = function(knex, Promise) {
           data: '6/1/2018',
           time: '4:00pm'
         }),
-        knex('users').insert({
-          id: 1, 
-          user_id: '456',
-          email: 'thurmanvogt@gmail.com',
-          password: 'sage',
-          user_img: 'http://us.cdn281.fansshare.com/photos/channingtatum/bfhgoi-ccaagvrc-large-fat-44506770.jpg',
-          bio: 'i like food',
-          rating: 'solid 7'
-        }),
         knex('pickup').insert({
           id: 1, 
           ride_id: 1,
           location_id: 1,
-          pickup_lat_lng: '40.7359, 73.991'
+          pickup_lat_lng: '40.7359, 73.991',
+          isShowing: false,
+          address: '2905 Inca St, Denver, CO 80202'
+        }),
+        knex('rides_passengers').insert({
+          id: 1,
+          ride_id: 1,
+          passenger_id: 1,
+          location_id: 1
         })
       ]);
     });
