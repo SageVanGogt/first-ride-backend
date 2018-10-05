01a124e06ec7ea730557651b56b7faf34fd76efa
Sage Vogt <thurmanvogt@gmail.com>
2018-10-04 21:31:08 -0600

    Update server missing port set for dev env

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+app.set('port', process.env.PORT || 3000);
 
 app.use(function(request, response, next) {
   response.header("Access-Control-Allow-Origin", "*");
